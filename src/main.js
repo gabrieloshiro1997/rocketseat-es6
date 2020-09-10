2b3ecfd44b558633d80a3400370dba97cdf6cb49
Gabriel Oshiro <gabriel.oshiro@cbyk.com.br>
2020-09-10 14:32:49 -0300

    Adicionar repositorios

--- src/main.js
+++ src/main.js
@@ -1,15 +1,28 @@
-import axios from 'axios';
+class App {
+	constructor() {
+		this.repositories = [];
 
-class Api {
-	static async getUserInfo(username) {
-		try {
-			const response = await axios.get(`https://api.github.com/users/${username}`);
-			console.log(response);
-		} catch(err) {
-			console.warn('Erro na API');
-		}
+		this.formEl = document.getElementById('repo-form');
 
+		this.registerHandlers();
+	}
+
+	registerHandlers() {
+		this.formEl.onsubmit = event => this.addRepository(event);
+	}
+
+	addRepository(event) {
+		event.preventDefault();
+
+		this.repositories.push({
+			name: 'gabrieloshiro1997',
+			description: 'teste',
+			avatar_url: 'https://avatars1.githubusercontent.com/u/46484835?v=4',
+			html_url: 'http://github.com/rocketseat/rocketseat.com.br'
+		});
+
+		console.log(this.repositories);
 	}
 }
 
-console.log(Api.getUserInfo('gabrieloshiro1997'));
+new App();
